8110255ecac893ce6f5cf829519938daaa66a920
Sejal Mali <msejal3732@gmail.com>
2025-12-10 19:54:37 -0500

    rect area peri

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+
+import arithmetic
+from geometry import calc_rect_area
+from geometry import calc_rect_peri
 print("Hello World!!")
+arithmetic.add(10,5)
+arithmetic.sub(10,5)
+
+len = int(input("Enter num1:"))
+br = int(input("Enter num2:"))
+
+calc_rect_area(len,br)
+calc_rect_peri(len,br)
